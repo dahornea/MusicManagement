279700ac1ee69dfa0f66e818ae17ce16cc538f77
913-Hornea-Dorian-Alexandru <alex.1375@yahoo.com>
2023-07-22 19:14:14 +0300

    Added DB inserts

--- DTOs/ArtistMoneyDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace DTOs
-{
-    public class ArtistMoneyDTO
-    {
-        public long? id { get; set; }
-        public string? Name { get; set; }
-        public double? MoneyEarned { get; set; }
-    }
-}
